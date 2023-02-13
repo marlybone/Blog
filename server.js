1a6d90554b2a74b6201337322a992db1e5a729fa
marlybone <41431476+marlybone@users.noreply.github.com>
2023-02-13 23:43:29 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const articleRouter = require('./routes/article')
+
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title:'test',
+        date: Date.now(),
+        description: 'This is a test'
+    }]
+    res.render('index', {article: articles})
+})
+
+app.listen(5000);
+
