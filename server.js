852a67e77b8338cc01c9e613e6cbc057e5b5c1f9
marlybone <41431476+marlybone@users.noreply.github.com>
2023-02-15 21:30:45 +0100

    Add untracked files

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
 const articleRouter = require('./routes/article')
+const mongoose = require('mongoose')
+
+
+// mongoose.set('strictQuery', false)
+// mongoose.connect('mongodb://localhost/blog', {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//       });
+
+    var authRouter = require('./routes/auth');
 
 
 app.set('view engine', 'ejs')
 
 app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
     const articles = [{
         title:'test',
-        date: Date.now(),
+        date: new Date(),
         description: 'This is a test'
     }]
     res.render('index', {articles: articles})
