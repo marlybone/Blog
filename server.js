6fce3d523cc3500151ee50552a68d275ccdb3ff4
marlybone <41431476+marlybone@users.noreply.github.com>
2023-02-20 23:27:26 +0100

    organising layout

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
 const express = require('express');
 const app = express();
-const articleRouter = require('./routes/article')
-const mongoose = require('mongoose')
 
+app.set('view engine', 'ejs');
 
-// mongoose.set('strictQuery', false)
-// mongoose.connect('mongodb://localhost/blog', {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//       });
-
-    var authRouter = require('./routes/auth');
-
-
-app.set('view engine', 'ejs')
-
-app.use('/articles', articleRouter)
-app.use(express.urlencoded({ extended: false }))
-app.use('/auth', authRouter);
-
-app.get('/', (req, res) => {
-    const articles = [{
-        title:'test',
-        date: new Date(),
-        description: 'This is a test'
-    }]
-    res.render('index', {articles: articles})
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
 })
 
-app.listen(5000);
 
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
