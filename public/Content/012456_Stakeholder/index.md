5dcd11eba1fd37694710b018e50de27abea5e8b6
marlybone <41431476+marlybone@users.noreply.github.com>
2023-02-24 23:02:20 +0100

    added tracked on some files

---
+++ public/Content/012456_Stakeholder/index.md
@@ -0,0 +1,13 @@
+---
+date: 24-02-2023
+title: Building An Interviewable C.V
+template: post
+thumbnail: ../../imgs/job.jpg
+tags:
+  - Personal
+  - Career
+---
+
+#How to Build an Interviewable C.V 
+
+etc etc. 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ejs}",
@@ -11,7 +12,32 @@ module.exports = {
       'Merri': 'Merriweather',
       'Inter': 'Inter',
     },
-    extend: {},
+    extend: {
+      colors: {
+        slate: colors.slate,
+        gray: colors.gray,
+        zinc: colors.zinc,
+        neutral: colors.neutral,
+        stone: colors.stone,
+        red: colors.red,
+        orange: colors.orange,
+        amber: colors.amber,
+        yellow: colors.yellow,
+        lime: colors.lime,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        sky: colors.sky,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuchsia: colors.fuchsia,
+        pink: colors.pink,
+        rose: colors.rose,
   },
+  },
+},
   plugins: [],
 }
