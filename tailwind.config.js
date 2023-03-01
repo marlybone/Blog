1e595a6274eb5b96305634f48e69c82aa8b96106
marlybone <41431476+marlybone@users.noreply.github.com>
2023-03-01 23:07:46 +0100

    added 404 and loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,30 +13,11 @@ module.exports = {
       'Inter': 'Inter',
     },
     extend: {
-      colors: {
-        slate: colors.slate,
-        gray: colors.gray,
-        zinc: colors.zinc,
-        neutral: colors.neutral,
-        stone: colors.stone,
-        red: colors.red,
-        orange: colors.orange,
-        amber: colors.amber,
-        yellow: colors.yellow,
-        lime: colors.lime,
-        green: colors.green,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        cyan: colors.cyan,
-        sky: colors.sky,
-        blue: colors.blue,
-        indigo: colors.indigo,
-        violet: colors.violet,
-        purple: colors.purple,
-        fuchsia: colors.fuchsia,
-        pink: colors.pink,
-        rose: colors.rose,
-  },
+      keyframes: {
+        fadein: {
+          
+        }
+      }
   },
 },
   plugins: [],
